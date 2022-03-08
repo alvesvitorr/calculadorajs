1ac28ef7d47c90c2be2a9335a41c5b05b004bd51
alves <vitor.rosalves@outlook.com>
2022-03-08 02:06:52 -0300

    branch switchCase

--- calculadora.js
+++ calculadora.js
@@ -2,7 +2,7 @@ function calculadora() {
 const operacao =Number(prompt('Escolha uma operação: \n\n 1- Soma(+) \n 2- Substração(-) \n 3- Multiplicação (*)\n 4- Divisão real(/)\n 5- Divisão Inteira(%)\n 6- Potenciação(**)'));
 
     if(!operacao || operacao >= 7){
-        alert('Operação Inválida!');
+        alert('Atenção: Operação Inválida!');
         calculadora();
     } else {
 
@@ -11,7 +11,7 @@ const operacao =Number(prompt('Escolha uma operação: \n\n 1- Soma(+) \n 2- Sub
     let resultado;
 
     if (!n1 || !n2){
-        alert('Erro, Parametros inválidos')
+        alert('Erro: Parametros inválidos!')
         calculadora();
     } else{
         function soma() {
@@ -64,22 +64,41 @@ const operacao =Number(prompt('Escolha uma operação: \n\n 1- Soma(+) \n 2- Sub
     
     }
 // Verificador de operações digitadas
-    if (operacao ==1){
-        soma();
-    } else if (operacao == 2){
-        subtracao();
-    } else if (operacao == 3){
-        multiplicacao();
-    } else if (operacao == 4){
-        divisaoReal();
-    } else if (operacao == 5){
-        divisaoInteira();
-    } else if (operacao == 6){
-        potenciacao();
+    // if (operacao ==1){
+    //     soma();
+    // } else if (operacao == 2){
+    //     subtracao();
+    // } else if (operacao == 3){
+    //     multiplicacao();
+    // } else if (operacao == 4){
+    //     divisaoReal();
+    // } else if (operacao == 5){
+    //     divisaoInteira();
+    // } else if (operacao == 6){
+    //     potenciacao();
         
+    // }
     }
-    }
+    switch (operacao){
+        case 1:
+            soma();
+            break;
+        case 2:
+            subtracao();
+            break;
+        case 3:
+            multiplicacao();
+            break;
+        case 4:
+            divisaoReal();
+            break;
+        case 5:
+            divisaoInteira();
+            break;
+        case 6:
+            potenciacao();
 
+    }
     
 }
 
